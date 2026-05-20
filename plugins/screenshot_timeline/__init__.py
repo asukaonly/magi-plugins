da11c7e09a74958b58a0e241131cda8230b7d0e4
asuka <luyanliang321123@gmail.com>
2026-05-21 02:04:13 +0800

    feat(screenshot_timeline): plugin manifest with settings + activation flow

---
+++ plugins/screenshot_timeline/__init__.py
@@ -0,0 +1,6 @@
+"""Screenshot timeline plugin for magi."""
+from __future__ import annotations
+
+from .plugin import DEFAULT_SETTINGS, ScreenshotTimelinePlugin
+
+__all__ = ["ScreenshotTimelinePlugin", "DEFAULT_SETTINGS"]
